e7bb4a82495dec94d638bee30e63a5f324087648
Marius Bogoevici <marius.bogoevici@gmail.com>
2009-10-09 22:23:47 +0000

    create new module for the implementation of the interceptors library
    git-svn-id: http://svn.jboss.org/repos/jbossas/projects/interceptors/trunk@94626 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ interceptors-impl/src/main/java/org/jboss/interceptor/proxy/DirectClassInterceptionHandler.java
@@ -0,0 +1,162 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.interceptor.proxy;
+
+import org.jboss.interceptor.model.InterceptionType;
+import org.jboss.interceptor.model.InterceptorClassMetadata;
+import org.jboss.interceptor.registry.InterceptorClassMetadataRegistry;
+import org.jboss.interceptor.InterceptorException;
+import org.jboss.interceptor.util.ReflectionUtils;
+
+import javax.interceptor.InvocationContext;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * @author <a href="mailto:mariusb@redhat.com">Marius Bogoevici</a>
+ */
+public class DirectClassInterceptionHandler<I> implements InterceptionHandler
+{
+
+   private final Object interceptorInstance;
+
+   private InterceptorClassMetadata interceptorMetadata;
+
+   private Class<?> clazz;
+
+   public DirectClassInterceptionHandler(Object interceptorInstance, Class<?> clazz)
+   {
+      if (interceptorInstance == null)
+         throw new IllegalArgumentException("Interceptor instance cannot be null");
+
+      this.clazz = (clazz == null) ? interceptorInstance.getClass() : clazz;
+      this.interceptorInstance = interceptorInstance;
+      this.interceptorMetadata = InterceptorClassMetadataRegistry.getRegistry().getInterceptorClassMetadata(this.clazz);
+
+   }
+
+   public DirectClassInterceptionHandler(Class<?> simpleInterceptorClass)
+   {
+
+      if (simpleInterceptorClass == null)
+         throw new IllegalArgumentException("Class must not be null");
+
+      this.clazz = simpleInterceptorClass;
+      try
+      {
+         this.interceptorInstance = simpleInterceptorClass.newInstance();
+      } catch (Exception e)
+      {
+         throw new InterceptorException("Cannot create interceptor instance:", e);
+      }
+      this.interceptorMetadata = InterceptorClassMetadataRegistry.getRegistry().getInterceptorClassMetadata(this.clazz);
+
+   }
+
+   public Object invoke(Object target, InterceptionType interceptionType, InvocationContext invocationContext) throws Exception
+   {
+      List<Method> methods = interceptorMetadata.getInterceptorMethods(interceptionType);
+      if (methods != null)
+      {
+         DelegatingInvocationContext delegatingInvocationContext = new DelegatingInvocationContext(invocationContext, interceptorInstance, methods);
+         return delegatingInvocationContext.proceed();
+      } else
+         throw new InterceptorException(target.toString() + " was requested to perform " + interceptionType.name() + " but no such method is defined on it");
+   }
+
+
+   public boolean handles(Class<?> clazz)
+   {
+      return this.clazz.equals(clazz);
+   }
+
+   public class DelegatingInvocationContext implements InvocationContext
+   {
+
+      private InvocationContext delegateInvocationContext;
+
+      private Object targetObject;
+
+      private Queue<Method> invocationQueue;
+
+      public DelegatingInvocationContext(InvocationContext delegateInvocationContext, Object targetObject, List<Method> methods)
+      {
+         this.delegateInvocationContext = delegateInvocationContext;
+         this.targetObject = targetObject;
+         this.invocationQueue = new ConcurrentLinkedQueue<Method>(methods);
+      }
+
+      public Map<String, Object> getContextData()
+      {
+         return delegateInvocationContext.getContextData();
+      }
+
+      public Method getMethod()
+      {
+         return delegateInvocationContext.getMethod();
+      }
+
+      public Object[] getParameters()
+      {
+         return delegateInvocationContext.getParameters();
+      }
+
+      public Object getTarget()
+      {
+         return delegateInvocationContext.getTarget();
+      }
+
+      public Object proceed() throws Exception
+      {
+         if (!invocationQueue.isEmpty())
+         {
+
+            Method interceptorMethod = invocationQueue.remove();
+            ReflectionUtils.ensureAccessible(interceptorMethod);
+            try
+            {
+               if (interceptorMethod.getParameterTypes().length == 0)
+                  return interceptorMethod.invoke(targetObject);
+               else
+                  return interceptorMethod.invoke(targetObject, this);
+            }
+            catch (InvocationTargetException e)
+            {
+               if (e.getCause() instanceof Exception)
+                  throw (Exception)e.getCause();
+               else
+                  throw new InterceptorException(e);
+            }
+         } else
+         {
+            return delegateInvocationContext.proceed();
+         }
+      }
+
+      public void setParameters(Object[] params)
+      {
+         delegateInvocationContext.setParameters(params);
+      }
+   }
+
+}
+
---
+++ interceptors-impl/src/test/java/org/jboss/interceptors/proxy/InterceptionTest.java
@@ -0,0 +1,153 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.interceptors.proxy;
+
+import org.jboss.interceptor.model.InterceptionModelBuilder;
+import org.jboss.interceptor.model.InterceptionModel;
+import org.jboss.interceptor.proxy.InterceptorProxyCreator;
+import org.jboss.interceptor.proxy.InterceptorProxyCreatorImpl;
+import org.jboss.interceptor.proxy.DirectClassInterceptionHandlerFactory;
+import org.jboss.interceptor.registry.InterceptorRegistry;
+import org.jboss.interceptor.util.InterceptionUtils;
+import org.jboss.interceptors.proxy.FootballTeam;
+import org.jboss.interceptors.proxy.InterceptorTestLogger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+
+/**
+ * @author <a href="mailto:mariusb@redhat.com">Marius Bogoevici</a>
+ */
+public class InterceptionTest
+{
+   private static final String TEAM_NAME = "Ajax Amsterdam";
+
+   private String[] expectedLoggedValues = {
+         "org.jboss.interceptors.proxy.InterceptionTest$MyFirstInterceptor_postConstruct",
+         "org.jboss.interceptors.proxy.InterceptionTest$MyFirstInterceptor_aroundInvokeBefore",
+         "org.jboss.interceptors.proxy.InterceptionTest$MySecondInterceptor_aroundInvokeBefore",
+         "org.jboss.interceptors.proxy.FootballTeam_aroundInvokeBefore",
+         "org.jboss.interceptors.proxy.FootballTeam_getName",
+         "org.jboss.interceptors.proxy.FootballTeam_aroundInvokeAfter",
+         "org.jboss.interceptors.proxy.InterceptionTest$MySecondInterceptor_aroundInvokeAfter",
+         "org.jboss.interceptors.proxy.InterceptionTest$MyFirstInterceptor_aroundInvokeAfter",
+         "org.jboss.interceptors.proxy.InterceptionTest$MySecondInterceptor_preDestroy"
+
+   };
+   private InterceptionModel<Class<?>,Class<?>> interceptionModel;
+   private InterceptorRegistry<Class<?>,Class<?>> interceptorRegistry;
+
+   @Before
+   public void resetLogAndSetupClasses() throws Exception
+   {
+      InterceptorTestLogger.reset();
+
+      InterceptionModelBuilder<Class<?>, Class<?>> builder = InterceptionModelBuilder.newBuilderFor(FootballTeam.class, (Class)Class.class);
+
+      builder.interceptAroundInvoke(FootballTeam.class.getMethod("getName")).with(MyFirstInterceptor.class, MySecondInterceptor.class);
+      builder.interceptPostConstruct().with(MyFirstInterceptor.class);
+      builder.interceptPreDestroy().with(MySecondInterceptor.class);
+      interceptionModel = builder.build();
+      this.interceptorRegistry = new InterceptorRegistry<Class<?>, Class<?>>();
+      this.interceptorRegistry.registerInterceptionModel(FootballTeam.class, interceptionModel);
+
+   }
+
+   @Test
+   @Ignore
+   public void testInterceptionWithInstrumentedClass() throws Exception
+   {
+
+      //FootballTeam proxy = interceptorProxyCreator.createInstrumentedInstance(FootballTeam.class, new Class<?>[]{String.class}, new Object[]{TEAM_NAME});
+      //FootballTeam proxy = interceptorProxyCreator.createProxyFromInstance(new FootballTeam(TEAM_NAME), FootballTeam.class);
+      //executeAssertionsOnProxy(proxy);
+
+   }
+
+
+   @Test
+   public void testInterceptionWithProxifiedObject() throws Exception
+   {
+      FootballTeam proxy = InterceptionUtils.proxifyInstance(new FootballTeam(TEAM_NAME), FootballTeam.class, interceptorRegistry, new DirectClassInterceptionHandlerFactory());
+      executeAssertionsOnProxy(proxy);
+   }
+
+   private void executeAssertionsOnProxy(FootballTeam proxy)
+   {
+      InterceptionUtils.executePostConstruct(proxy);
+      Assert.assertEquals(TEAM_NAME, proxy.getName());
+      InterceptionUtils.executePredestroy(proxy);
+      Object[] logValues = InterceptorTestLogger.getLog().toArray();
+      Assert.assertArrayEquals(iterateAndDisplay(logValues), expectedLoggedValues, logValues);
+   }
+
+   private String iterateAndDisplay(Object[] logValues)
+   {
+      StringBuffer buffer = new StringBuffer();
+      for (Object logValue: logValues)
+      {
+         buffer.append(logValue.toString()).append("\n");
+      }
+      return buffer.toString();
+   }
+
+
+   public static class MyFirstInterceptor
+   {
+
+      @AroundInvoke
+      private Object doAround(InvocationContext invocationContext) throws Exception
+      {
+         InterceptorTestLogger.add(MyFirstInterceptor.class, "aroundInvokeBefore");
+         Object result = invocationContext.proceed();
+         InterceptorTestLogger.add(MyFirstInterceptor.class, "aroundInvokeAfter");
+         return result;
+      }
+
+      @PostConstruct
+      public void doAfterConstruction(InvocationContext invocationContext) throws Exception
+      {
+         InterceptorTestLogger.add(MyFirstInterceptor.class, "postConstruct");
+      }
+   }
+
+   public static class MySecondInterceptor extends MyFirstInterceptor
+   {
+      @AroundInvoke
+      private Object doAround(InvocationContext invocationContext) throws Exception
+      {
+         InterceptorTestLogger.add(MySecondInterceptor.class, "aroundInvokeBefore");
+         Object result = invocationContext.proceed();
+         InterceptorTestLogger.add(MySecondInterceptor.class, "aroundInvokeAfter");
+         return result;
+      }
+
+      @PreDestroy
+      private void doneHere(InvocationContext invocationContext) throws Exception
+      {
+         InterceptorTestLogger.add(MySecondInterceptor.class, "preDestroy");
+      }
+   }
+}
+
